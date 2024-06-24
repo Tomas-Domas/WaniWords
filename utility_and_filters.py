e609ac7d04ca5839d8e771a431a93a8e348e6e69
BigDomas <tomatitus2@gmail.com>
2024-06-24 23:46:52 +0900

    Separate word list generation and filtering into a separate class. Separate generating list of words and filtering out known vocab into their own functions

---
+++ utility_and_filters.py
@@ -0,0 +1,99 @@
+import json
+
+from wanikani import WaniKaniHandler
+
+_FREQUENCY_LIST_FILE = "Frequency_List.json"
+_FREQ_SOURCE_FILE = "BCCWJ_frequencylist_suw_ver1_0.tsv"
+_BLACKLISTED_WORD_TYPES = ["助詞", "助動詞", "接尾辞", "数詞", "固有名詞"]
+_KANA_LIST = [
+    'ぁ', 'あ', 'ぃ', 'い', 'ぅ', 'う', 'ゔ', 'ぇ', 'え', 'ぉ', 'お', 'ゕ', 'か', 'が', 'き', 'ぎ', 'く', 'ぐ', 'ゖ', 'け', 'げ',
+    'こ', 'ご', 'さ', 'ざ', 'し', 'じ', 'す', 'ず', 'せ', 'ぜ', 'そ', 'ぞ', 'た', 'だ', 'ち', 'ぢ', 'っ', 'つ', 'づ', 'て', 'で',
+    'と', 'ど', 'な', 'に', 'ぬ', 'ね', 'の', 'は', 'ば', 'ぱ', 'ひ', 'び', 'ぴ', 'ふ', 'ぶ', 'ぷ', 'へ', 'べ', 'ぺ', 'ほ', 'ぼ',
+    'ぽ', 'ま', 'み', 'む', 'め', 'も', 'ゃ', 'や', 'ゅ', 'ゆ', 'ょ', 'よ', 'ら', 'り', 'る', 'れ', 'ろ', 'ゎ', 'わ', 'ゐ', 'ゑ',
+    'を', 'ん', 'ァ', 'ア', 'ィ', 'イ', 'ゥ', 'ウ', 'ヴ', 'ェ', 'エ', 'ォ', 'オ', 'ヵ', 'カ', 'ガ', 'キ', 'ギ', 'ク', 'グ', 'ヶ',
+    'ケ', 'ゲ', 'コ', 'ゴ', 'サ', 'ザ', 'シ', 'ジ', 'ス', 'ズ', 'セ', 'ゼ', 'ソ', 'ゾ', 'タ', 'ダ', 'チ', 'ヂ', 'ッ', 'ツ', 'ヅ',
+    'テ', 'デ', 'ト', 'ド', 'ナ', 'ニ', 'ヌ', 'ネ', 'ノ', 'ハ', 'バ', 'パ', 'ヒ', 'ビ', 'ピ', 'フ', 'ブ', 'プ', 'ベ', 'ペ', 'ホ',
+    'ボ', 'ポ', 'マ', 'ミ', 'ム', 'メ', 'モ', 'ャ', 'ヤ', 'ュ', 'ユ', 'ョ', 'ヨ', 'ラ', 'リ', 'ル', 'レ', 'ロ', 'ヮ', 'ワ', 'ヷ',
+    'ヰ', 'ヸ', 'ヱ', 'ヹ', 'ヲ', 'ヺ', 'ン', '・', 'ー'
+]
+
+def generate_frequency_list_file() -> None:
+    """
+    Generate a Frequency List file from the BCCWJ database file
+    Filters out words that are categorized as any of the Blacklisted Types
+    """
+    with open(_FREQUENCY_LIST_FILE, "w") as frequency_list_file:
+        list_of_words = []
+        with open(_FREQ_SOURCE_FILE, "r", encoding='utf-8') as freq_source_file:
+            for line in freq_source_file:
+                data = line.split('\t')
+                word = data[2]
+                word_type = data[3]
+                word_is_blacklisted = False
+                for blacklisted_type in _BLACKLISTED_WORD_TYPES:
+                    if blacklisted_type in word_type:
+                        word_is_blacklisted = True
+                        break
+                if not word_is_blacklisted:
+                    list_of_words.append(word)
+        json.dump(
+            list_of_words[1:],
+            frequency_list_file,
+            indent=0
+        )
+
+def get_common_words_list(num_of_words: int) -> list[str]:
+    """
+    Generate a list of words from the frequency list file
+    :param num_of_words: The number of words to retrieve (e.g. 500 = the 500 most common words)
+    :return: List of words sorted by frequency according to the frequency list file
+    """
+    with open(_FREQUENCY_LIST_FILE, "r", encoding='utf-8') as frequency_list_file:
+        words_list = json.load(frequency_list_file)
+        if len(words_list) < num_of_words:  # Cap up_to_frequency to the length of word_list
+            print("Frequency list doesn't contain %d words. Could only retrieve %d.", (num_of_words, len(words_list)))
+            return words_list
+        else:
+            return words_list[0:num_of_words]
+
+def filter_out_known_words(list_of_words: list[str], wanikani_handler: WaniKaniHandler, invert_filter: bool = False) -> list[str]:
+    """
+    Removes words from the list that were learned through WaniKani
+    :param list_of_words: list of words to be filtered
+    :param wanikani_handler: WaniKaniHandler for the user
+    :param invert_filter: whether the filter should be inverted (i.e. filter out unknown words)
+    :return: new, filtered list of words
+    """
+    known_vocabulary = wanikani_handler.get_known_vocabulary_list()
+    new_list_of_words = []
+    for word in list_of_words:
+        if bool(word not in known_vocabulary) ^ invert_filter:
+            new_list_of_words.append(word)
+    return new_list_of_words
+
+def filter_out_unknown_kanji(list_of_words: list[str], wanikani_handler: WaniKaniHandler, invert_filter: bool = False) -> list[str]:
+    """
+    Removes words from the list that contain kanji not yet learned through WaniKani
+    :param list_of_words: list of words to be filtered
+    :param wanikani_handler: WaniKaniHandler for the user
+    :param invert_filter: whether the filter should be inverted (i.e. filter out words of only known kanji)
+    :return: new, filtered list of words
+    """
+    known_characters = _KANA_LIST + wanikani_handler.get_known_kanji_list()
+    new_list_of_words = []
+    if invert_filter is False:
+        for word in list_of_words:
+            for character in word:
+                if character not in known_characters:
+                    break
+            else:
+                new_list_of_words.append(word)
+    else:
+        for word in list_of_words:
+            for character in word:
+                if character not in known_characters:
+                    break
+            else:
+                continue
+            new_list_of_words.append(word)
+    return new_list_of_words
--- wanikani.py
+++ wanikani.py
@@ -1,7 +1,8 @@
 import json
 import requests
 
-WANIKANI_CACHE_FILE = "WaniKani Cache.json"
+WANIKANI_CACHE_FILE = "WaniKani_Cache.json"
+
 class WaniKaniHandler:
     def __init__(self, api_token):
         """
@@ -10,14 +11,14 @@ class WaniKaniHandler:
         """
         self._api_token = api_token
         with open(WANIKANI_CACHE_FILE, "r", encoding='utf-8') as cache_file:
-            self._data_cache_json = json.load(cache_file)
+            self._data_dictionary = json.load(cache_file)
 
     def _get_data_from_api(self, endpoint: str, parameters: dict[str, str]) -> list[dict]:
         """
         Wrapper for calling the WaniKani API. Packages the received data into a list
         :param endpoint: URL endpoint for the API request
         :param parameters: Parameters and Filters for the initial API request
-        :return: list of JSON objects received from the request
+        :return: List of JSON objects received from the request
         """
         data_array = []
         next_page = "https://api.wanikani.com/v2/" + endpoint
@@ -37,7 +38,8 @@ class WaniKaniHandler:
 
     def download_all_data(self) -> None:
         """
-        Downloads the subjects and assignments for both vocabulary and kanji
+        Downloads the subjects and assignments for both vocabulary and kanji.
+        Writes the downloaded data to the cache file
         """
         self.download_wanikani_vocabulary()
         self.download_wanikani_kanji()
@@ -48,6 +50,7 @@ class WaniKaniHandler:
         """
         Downloads all the WaniKani kanji subjects.
         Stored in a dictionary as a (subject_id : kanji_string) pair
+        Does not write the downloaded data to the cache file. For this, use write_cache()
         """
         kanji_subjects_list = self._get_data_from_api(
             endpoint="subjects",
@@ -59,13 +62,14 @@ class WaniKaniHandler:
         for kanji in kanji_subjects_list:
             id_to_kanji_dictionary[kanji["id"]] = kanji["data"]["characters"]
 
-        self._data_cache_json["all_kanji_subjects"] = id_to_kanji_dictionary
+        self._data_dictionary["all_kanji_subjects"] = id_to_kanji_dictionary
         print("Wanikani Kanji Subjects have been updated")
 
     def download_wanikani_vocabulary(self) -> None:
         """
         Downloads all the WaniKani vocabulary subjects
         Stored in a dictionary as a (subject_id : vocabulary_string) pair
+        Does not write the downloaded data to the cache file. For this, use write_cache()
         """
         vocabulary_subjects_list = self._get_data_from_api(
             endpoint="subjects",
@@ -77,13 +81,14 @@ class WaniKaniHandler:
         for vocabulary in vocabulary_subjects_list:
             id_to_vocabulary_dictionary[vocabulary["id"]] = vocabulary["data"]["characters"]
 
-        self._data_cache_json["all_vocabulary_subjects"] = id_to_vocabulary_dictionary
+        self._data_dictionary["all_vocabulary_subjects"] = id_to_vocabulary_dictionary
         print("Wanikani Vocabulary Subjects have been updated")
 
     def download_user_known_kanji(self) -> None:
         """
         Downloads user's kanji assignments that are Guru level or higher
         Stored in a dictionary as a (subject_id : srs_stage) pair
+        Does not write the downloaded data to the cache file. For this, use write_cache()
         """
         kanji_assignments_list = self._get_data_from_api(
             endpoint="assignments",
@@ -96,13 +101,14 @@ class WaniKaniHandler:
         for kanji in kanji_assignments_list:
             id_to_srs_dictionary[kanji["data"]["subject_id"]] = kanji["data"]["srs_stage"]
 
-        self._data_cache_json["user_kanji_assignments"] = id_to_srs_dictionary
+        self._data_dictionary["user_kanji_assignments"] = id_to_srs_dictionary
         print("User Kanji Assignments have been updated")
 
     def download_user_known_vocabulary(self) -> None:
         """
         Downloads user's vocabulary assignments that are Apprentice level or higher
         Stored in a dictionary as a (subject_id : srs_stage) pair
+        Does not write the downloaded data to the cache file. For this, use write_cache()
         """
         vocabulary_assignments_list = self._get_data_from_api(
             endpoint="assignments",
@@ -115,17 +121,37 @@ class WaniKaniHandler:
         for vocabulary in vocabulary_assignments_list:
             id_to_srs_dictionary[vocabulary["data"]["subject_id"]] = vocabulary["data"]["srs_stage"]
 
-        self._data_cache_json["user_vocabulary_assignments"] = id_to_srs_dictionary
+        self._data_dictionary["user_vocabulary_assignments"] = id_to_srs_dictionary
         print("User Vocabulary Assignments have been updated")
 
     def write_cache(self) -> None:
         """
-        Writes the downloaded data to the cache file
+        Writes the currently held data to the cache file
         """
         with open(WANIKANI_CACHE_FILE, "w", encoding='utf-8') as cache_file:
             json.dump(
-                self._data_cache_json,
+                self._data_dictionary,
                 cache_file,
                 indent=3,
                 ensure_ascii=False
             )
+
+    def get_known_kanji_list(self) -> list[str]:
+        """
+        Cross-references the user and wanikani data to produce a list of known kanji
+        :return: List containing unicode strings of kanji
+        """
+        known_kanji_list = []
+        for lesson_id in self._data_dictionary["user_kanji_assignments"]:
+            known_kanji_list.append(self._data_dictionary["all_kanji_subjects"][lesson_id])
+        return known_kanji_list
+
+    def get_known_vocabulary_list(self) -> list[str]:
+        """
+        Cross-references the user and wanikani data to produce a list of known vocabulary words
+        :return: List containing unicode strings of vocabulary words
+        """
+        known_vocabulary_list = []
+        for lesson_id in self._data_dictionary["user_vocabulary_assignments"]:
+            known_vocabulary_list.append(self._data_dictionary["all_vocabulary_subjects"][lesson_id])
+        return known_vocabulary_list
