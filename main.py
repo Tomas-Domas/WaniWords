3e237e589ec5e1d9d2e716afc1d8871ef418ba6a
BigDomas <tomatitus2@gmail.com>
2024-06-25 22:53:58 +0900

    Edit main to pull wanikani data, generate vocab list, and add it to JPDB deck

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-import json
-
 import utility_and_filters
 from wanikani import WaniKaniHandler
+from jpdb import JPDBHandler
 from config import WANIKANI_API_TOKEN, JPDB_API_TOKEN
 
 
 def main():
-    # # Regenerate file from the database
-    utility_and_filters.generate_frequency_list_file()
-
     wk_handler = WaniKaniHandler(WANIKANI_API_TOKEN)
-    # wk_handler.download_all_data()
-    # wk_handler.write_cache()
+    jpdb_handler = JPDBHandler(JPDB_API_TOKEN)
+
+    # utility_and_filters.generate_frequency_list_file()  # Regenerate file from the database
+    # wk_handler.download_all_data()  # Download data from wanikani and write to cache file
 
     words_list = utility_and_filters.get_common_words_list(1000)
-    print("Generated initial words list of [%d]:\n" % len(words_list) + str(words_list))
-    words_list = utility_and_filters.filter_out_unknown_kanji(words_list, wk_handler, invert_filter=False)
-    print("Filtered out unknown kanji down to [%d]:\n" % len(words_list) + str(words_list))
-    words_list = utility_and_filters.filter_out_known_words(words_list, wk_handler, invert_filter=False)
+    words_list = utility_and_filters.filter_out_unknown_kanji(words_list, wk_handler)
+    words_list = utility_and_filters.filter_out_known_words(words_list, wk_handler)
     print("Filtered out known words down to [%d]:\n" % len(words_list) + str(words_list))
+
+    jpdb_handler.add_vocabulary_to_waniwords_deck(words_list)
 
 
 if __name__ == "__main__":
