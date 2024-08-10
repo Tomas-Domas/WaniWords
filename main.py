0917ff9f8e068409bb41ae3fbefe439cf6c8ca53
Tomas Mejia <tomatitus2@gmail.com>
2024-08-09 23:47:31 -0400

    Add debugging console logs
    Print information as the pipeline is run,
    clean up the main a little bit

--- main.py
+++ main.py
@@ -7,16 +7,32 @@ from config import WANIKANI_API_TOKEN, JPDB_API_TOKEN
 def main():
     wk_handler = WaniKaniHandler(WANIKANI_API_TOKEN)
     jpdb_handler = JPDBHandler(JPDB_API_TOKEN)
-
+    # print("Generating Frequency List file from database...")
     # utility_and_filters.generate_frequency_list_file()  # Regenerate file from the database
+    # print("Downloading WaniKani user data...")
     # wk_handler.download_all_data()  # Download data from wanikani and write to cache file
 
-    words_list = utility_and_filters.get_common_words_list(1000)
-    words_list = utility_and_filters.filter_out_unknown_kanji(words_list, wk_handler)
-    words_list = utility_and_filters.filter_out_known_words(words_list, wk_handler)
-    print("Filtered out known words down to [%d]:\n" % len(words_list) + str(words_list))
+    NUM_OF_WORDS = 500
+    print("Generating word list of size %d..." % NUM_OF_WORDS)
+    words_list = utility_and_filters.generate_frequent_words(NUM_OF_WORDS)
 
+    print("Filtering out unkown kanji...", end="\t")
+    words_list = utility_and_filters.filter_out_unknown_kanji(words_list, wk_handler)
+    print(len(words_list), "words remaining.")
+
+    print("Filtering out known words... ", end="\t")
+    words_list = utility_and_filters.filter_out_known_words(words_list, wk_handler)
+    print(len(words_list), "words remaining.")
+
+    print("Generated list:")
+    for word in words_list:
+        print(word, end=" ")
+    print()
+
+    print("Adding words to JPDB deck...")
     jpdb_handler.add_vocabulary_to_waniwords_deck(words_list)
+
+    print("Finished!")
 
 
 if __name__ == "__main__":
